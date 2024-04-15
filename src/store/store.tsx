296c2026ae1fe2a4efd681b5318e1ccf8268cc3c
MinjooKim <kingwinju@gmail.com>
2024-04-15 19:31:08 +0900

    Setting: implement zustand store setup

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,6 +1,6 @@
 import create from 'zustand';
 
-const useStore = create(set => ({
+const useStore = create((set) => ({
   count: 0,
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
