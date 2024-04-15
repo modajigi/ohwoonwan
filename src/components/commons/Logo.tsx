c22cf9adae089b87c68d49ffa1c0fb16d2efcf46
SangJune-Kim <ghfnsgldl@gmail.com>
2024-04-15 19:42:45 +0900

    [Fixed] import fix

--- src/components/commons/Logo.tsx
+++ src/components/commons/Logo.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/" passHref>
-      <Image src="/apple.png" alt="logo" width={40} height={40} />
+      <Image src="/images/apple.png" alt="logo" width={40} height={40} />
     </Link>
   );
 }
